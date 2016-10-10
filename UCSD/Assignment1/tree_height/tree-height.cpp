b84d194d8329b1d17e8ca9a8b4e5151225bff0f9
HilbertSpaces <Jktecon@gmail.com>
2016-10-10 16:23:23 -0400

    Height

--- UCSD/Assignment1/tree_height/tree-height.cpp
+++ UCSD/Assignment1/tree_height/tree-height.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 class TreeHeight {
   int n;             
@@ -13,20 +14,43 @@ public:
     for (int i = 0; i < n; i++)
       std::cin >> parent[i];
   }
-
   int compute_height() {
     // Replace this code with a faster implementation
-    int maxHeight = 0;
+    /*int maxHeight = 0;
     for (int vertex = 0; vertex < n; vertex++) {
       int height = 0;
       for (int i = vertex; i != -1; i = parent[i])
         height++;
       maxHeight = std::max(maxHeight, height);
     }
-    return maxHeight;
+    return maxHeight;*/
+  std::vector<std::vector<int> > tree;
+  tree.resize(n);
+  for(int i=0; i<n; i++)
+  {
+	  tree[i][0]=i;
+	  tree[i][1]=-2;
+	  tree[i][2]=-2;
+  }
+  int next;
+  for(int i=0; i<n; i++)
+  {
+	  if(parent[i]==-1)
+		  next=i;
+	  else if(tree[parent[i]][1]==-2)
+		  tree[parent[i]][1]=parent[i];
+	  else
+		  tree[parent[i]][2]=parent[i];
+  }
+  return Height(tree,next,1);
+  }
+  int Height(std::vector<std::vector<int> > tree,int next,int side)
+  {
+	  if(tree[next][side]==-2)
+		  return 0;
+	  return 1+ std::max(Height(tree,tree[next][side],1),Height(tree,tree[next][side],2));
   }
 };
-
 int main() {
   std::ios_base::sync_with_stdio(0);
   TreeHeight tree;
